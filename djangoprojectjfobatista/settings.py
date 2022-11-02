248a4f0ad3f5defbec23455796eabaed7f6862ff
jfobatista <42Glsjhsrtljoao!>
2022-11-01 22:06:39 -0300

    Atualizando nome projeto

--- djangoprojectjfobatista/settings.py
+++ djangoprojectjfobatista/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pp1oa2gu5a%j(#kd_9qmkq_+jmgb2s6is)8o(*&-rbks1sa1r9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
