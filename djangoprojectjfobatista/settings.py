89d171ac84690b19c0f014ed8d1696c15b80eb63
jfobatista <42Glsjhsrtljoao!>
2022-11-02 20:21:37 -0300

    Extraindo valor de chave secreta
    close #17

--- djangoprojectjfobatista/settings.py
+++ djangoprojectjfobatista/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pp1oa2gu5a%j(#kd_9qmkq_+jmgb2s6is)8o(*&-rbks1sa1r9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
