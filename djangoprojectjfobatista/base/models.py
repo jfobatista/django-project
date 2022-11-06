ab39f10293dd1a2fe9fac635e3a29898c8e1a1a5
jfobatista <42Glsjhsrtljoao!>
2022-11-06 17:02:24 -0300

    Configuração do UserManager para realizar a criação de usuários e de super usuários
    close #35

--- djangoprojectjfobatista/base/models.py
+++ djangoprojectjfobatista/base/models.py
@@ -1,9 +1,40 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -13,12 +44,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
 
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), unique=True)
+    email = models.EmailField(_("email address"), unique=True) # usuario único com um email
+    # propriedade que define quais usuários podem acessar o admin do django
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
         help_text=_("Designates whether the user can log into this admin site."),
     )
+    # propriedade que define o usuário que pode logar dentro do sistema
     is_active = models.BooleanField(
         _("active"),
         default=True,
@@ -29,9 +62,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
-    #objects = UserManager()
+    objects = UserManager()
 
     EMAIL_FIELD = "email"
+    # utilizar o email como username
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["email"]
 
