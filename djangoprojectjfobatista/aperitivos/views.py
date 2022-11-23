cd682ab47b5a1a7180f6e84cd2b16ae93be2b4cd
jfobatista <42Glsjhsrtljoao!>
2022-11-23 18:48:12 -0300

    Adicionando filtro default para o título dos vídeos

--- djangoprojectjfobatista/aperitivos/views.py
+++ djangoprojectjfobatista/aperitivos/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 def video(request, slug):
     videos = {
         'bituca': {
+            'title': 'Cards - Bituquinha',
             'titulo': 'Bituquinha',
             'id': 770026394
         },
         'peek-inside': {
+            'title': 'Cards - Peek Inside',
             'titulo': 'Peek Inside',
             'id': 774443580
         },
