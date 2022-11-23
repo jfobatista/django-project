8eebea7bec1d95acf78d4509ff4c65267cbaec92
jfobatista <42Glsjhsrtljoao!>
2022-11-23 19:06:22 -0300

    Utilizando a tag for para mostrar os cards
    close #61

--- djangoprojectjfobatista/aperitivos/urls.py
+++ djangoprojectjfobatista/aperitivos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from djangoprojectjfobatista.aperitivos.views import video
+from djangoprojectjfobatista.aperitivos.views import video, indice
 
 app_name = 'aperitivos'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
+    path('', indice, name='indice'),
 ]
--- djangoprojectjfobatista/aperitivos/views.py
+++ djangoprojectjfobatista/aperitivos/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 
 
-def video(request, slug):
-    videos = {
-        'bituca': {
-            'title': 'Cards - Bituquinha',
-            'titulo': 'Bituquinha',
-            'id': 770026394
-        },
-        'peek-inside': {
-            'title': 'Cards - Peek Inside',
-            'titulo': 'Peek Inside',
-            'id': 774443580
-        },
+videos = [
+    {
+        'slug': 'bituca',
+        'title': 'Cards - Bituquinha',
+        'titulo': 'Bituquinha',
+        'id': 770026394
+    },
+    {
+        'slug': 'peek-inside',
+        'title': 'Cards - Peek Inside',
+        'titulo': 'Peek Inside',
+        'id': 774443580
+    },
+]
 
-    }
-    video = videos[slug]
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
+
+def video(request, slug):
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
 
