2284f66f4dde028a696746a039e2464536af5d9a
jfobatista <42Glsjhsrtljoao!>
2022-11-27 19:07:47 -0300

    criando testes com banco de dados, utilizando da biblioteca model_mommy para criar valores no banco de dados automaticamente para realizarmos os testes
    close #67

--- djangoprojectjfobatista/aperitivos/views.py
+++ djangoprojectjfobatista/aperitivos/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from djangoprojectjfobatista.aperitivos.models import Video
 
-videos = [
-    Video(slug='bituca', title='Cards - Bituquinha', titulo='Bituquinha', vimeo_id='770026394'),
-    Video(slug='peek-inside', title='Cards - Peek Inside', titulo='Peek Inside', vimeo_id='774443580'),
-]
-
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    video = Video.objects.get(slug=slug)
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
