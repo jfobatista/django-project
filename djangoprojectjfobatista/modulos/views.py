55042840b513ba95c4bbfb89db632ecc661ee990
jfobatista <42Glsjhsrtljoao!>
2022-12-11 15:37:52 -0300

    Criando página de detalhes sobre módulos close #88

--- djangoprojectjfobatista/modulos/views.py
+++ djangoprojectjfobatista/modulos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from djangoprojectjfobatista.modulos import fachada
 
 
 def detalhe(request, slug):
-    pass
+    modulo = fachada.encontrar_modulo(slug)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
