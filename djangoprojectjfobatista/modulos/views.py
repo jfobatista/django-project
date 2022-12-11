b050c8a7884423c71ed9c1a22c1c0b7eeff2c330
jfobatista <42Glsjhsrtljoao!>
2022-12-11 16:00:54 -0300

    Relacionamento um para muitos close #90

--- djangoprojectjfobatista/modulos/views.py
+++ djangoprojectjfobatista/modulos/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 from djangoprojectjfobatista.modulos import fachada
 
 
 def detalhe(request, slug):
     modulo = fachada.encontrar_modulo(slug)
-    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
+    aulas = fachada.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
